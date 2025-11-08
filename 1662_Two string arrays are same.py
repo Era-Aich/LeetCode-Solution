f9fe7fdd6c92a7c6aff1a7383243e1168a8931f8
Era Aich <45397193+Era-Aich@users.noreply.github.com>
2025-11-08 11:35:07 +0600

    Add files via upload

---
+++ 1662_Two string arrays are same.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  8 11:34:08 2025
+
+@author: asus
+"""
+
+w1 = list(input().strip().split())
+w2 = list(input().strip().split())
+p = ''.join(w1)
+q=''.join(w2)
+
+if p==q:
+    print("True")
+else:
+    print("False")
